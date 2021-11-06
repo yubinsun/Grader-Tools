201eb81e37b27d6900fe3b02cfbf41099a2a4d83
Yubin Sun <sunyubin982@gmail.com>
2021-11-06 09:41:55 -0700

    add to extract_error_sub: if dst folder not exist; if one turnin 2 copies of submission

--- util/extract_error_sub.py
+++ util/extract_error_sub.py
@@ -4,12 +4,15 @@ import  os,shutil
 from util import get_dir
 
 # 6 digits submissions is unique
-id = ["252841", "290529", "255139", "251673", "285934",
-"253196", "255644", "280027", "223660", "236721",
-"250777", "285377", "285469", "254916"]
+id = ["000000"]
 
-src = "/home/yb/112L_W21/lab4/submissions/"
-dst = "/home/yb/112L_W21/Lab4/error_sub/"
+src = "/home/yb/sambashare/F21-lab1/error_sub/"
+dst = "/home/yb/sambashare/F21-lab1/error_sub2/"
+
+os.system("mkdir " + dst)
+print("mkdir " + dst)
+
+processed = []
 
 count = 0
 os.chdir(src)
@@ -19,8 +22,13 @@ for f in dir:
         ii = str(i)
 
         if ii in f:
-            os.system("cp -r "+os.path.join(src,f)+" " +dst+" ")
-            print("cp -r "+os.path.join(src,f)+" " +dst+" ")
+            os.system("cp -r "+os.path.join(src,f)+" " +os.path.join(dst,"")+" ")
+            print("cp -r "+os.path.join(src,f)+" " +os.path.join(dst,"")+" ")
             count+= 1
+            if i in processed:
+                # sometimes they turn in 2 copies
+                print("Duplicate! id:" + i)
+            processed.append(i)
+
 
 print(len(id),'/',count)
