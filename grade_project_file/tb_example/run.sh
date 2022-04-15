4b0a8e54169d8c97b4d816fb496150b22390c44f
Yubin Sun <sunyubin982@gmail.com>
2022-04-15 05:54:09 -0700

    Update run.sh

--- grade_project_file/tb_example/run.sh
+++ grade_project_file/tb_example/run.sh
@@ -1,6 +1,12 @@
 # For linux
 
+# add commands you want to run before the simulation
+# 
+
 # Pre process : find IF_pipe_stage instance name 
-python3 pre_proc.py
+# Pre process is only needed for lab 4 of 112L, a special case.
+# python3 pre_proc.py
 #
+
+# Vivado address and flags.
 /tools/Xilinx/Vivado/2020.1/bin/vivado -mode batch -source test.tcl -nolog -nojournal -notrace
