0878e12638b340ecec19ee2df48c40afb78ca3dc
Yubin Sun - D <sunyubin982@gmail.com>
2022-04-15 06:16:06 -0700

    remove unnecessary files

--- grade_project_file/tb_example/instruction_mem_grade.v
+++
@@ -1,5 +0,0 @@
-        // load to registers 1 to 10
-                                                        // instruction           alu result in hex       register content       mem content
-														
-        rom[0]  = 32'b00000000000000000000000000000000; // 
-                                                                                                           
--- grade_project_file/tb_example/tb_mips_32_sw.v
+++
@@ -1,2 +0,0 @@
-
-if(uut.#DMTOBEREPLACED#.ram[1]==32'h00000000) $display("*TESTFIELD2* =  	{1}"); else $display("*TESTFIELD2* =  	{0}");
